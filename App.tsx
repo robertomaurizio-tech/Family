a983f37421cd8a722d914205dafe88a58bc39ca6
robertomaurizio-tech <roberto.maurizio@gmail.com>
2026-02-22 01:32:00 +0100

    feat: Implement lock screen functionality
    Introduces a lock screen to protect sensitive financial data, requiring user interaction to unlock the application and access the main dashboard and other features.

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Dashboard from './components/Dashboard';
@@ -8,26 +8,32 @@ import CategoryManager from './components/CategoryManager';
 import ShoppingList from './components/ShoppingList';
 import SandroAccount from './components/SandroAccount';
 import Settings from './components/Settings';
+import LockScreen from './components/LockScreen';
 
 import CarManager from './components/CarManager';
 import MonthlyReport from './components/MonthlyReport';
 
 const App: React.FC = () => {
+  const [isLocked, setIsLocked] = useState(true);
+
   return (
-    <HashRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/spese" element={<ExpenseList />} />
-          <Route path="/lista-spesa" element={<ShoppingList />} />
-          <Route path="/conto-sandro" element={<SandroAccount />} />
-          <Route path="/categorie" element={<CategoryManager />} />
-          <Route path="/garage" element={<CarManager />} />
-          <Route path="/report/:year/:month" element={<MonthlyReport />} />
-          <Route path="/settings" element={<Settings />} />
-        </Routes>
-      </Layout>
-    </HashRouter>
+    <>
+      {isLocked && <LockScreen onUnlock={() => setIsLocked(false)} />}
+      <HashRouter>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/spese" element={<ExpenseList />} />
+            <Route path="/lista-spesa" element={<ShoppingList />} />
+            <Route path="/conto-sandro" element={<SandroAccount />} />
+            <Route path="/categorie" element={<CategoryManager />} />
+            <Route path="/garage" element={<CarManager />} />
+            <Route path="/report/:year/:month" element={<MonthlyReport />} />
+            <Route path="/settings" element={<Settings />} />
+          </Routes>
+        </Layout>
+      </HashRouter>
+    </>
   );
 };
 
